0839153d58fc7456bea6ee9316d9d620b2c30bb6
Cassiano Dantas <cassiano15dantas@gmail.com>
2019-08-18 00:06:06 -0300

    veja

--- application.py
+++ application.py
@@ -2,5 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html', nome='cassiano')
+
+@app.route("/usuarios")
+def cadastro():
+    with open('usuarios.txt', 'r') as arquivo:
+        conteudo = arquivo.readlines()
+
+    return conteudo
